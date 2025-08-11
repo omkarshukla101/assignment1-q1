c4ed0359ec3f3cd3a0df4655045218b0a6666433
omkarshukla101 <omkarshukla.os2@gmail.com>
2025-08-11 10:44:10 +0530

    Create Q4c.cpp

---
+++ Q4c.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"enter the number of rows of the array:";
+    cin>>m;
+    cout<<"enter the number of coloumns of the array:";
+    cin>>n;
+    int arr[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+        cout<<"enter the element with row: "<<i+1<<" and coloumn:"<<j+1<<" of the array:";
+        cin>>arr[i][j];
+    }
+    }
+    for(int j=0;j<n;j++){
+        for(int i=0;i<m;i++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+} 
